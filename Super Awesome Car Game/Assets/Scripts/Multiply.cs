3cc0c5251cdc347460f9eefc369b48cebc98da5b
Cossette <10838257@my.uvu.edu>
2021-01-30 18:51:06 -0800

    idk what I did but I did something

---
+++ Super Awesome Car Game/Assets/Scripts/Multiply.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Multiply : MonoBehaviour
+{
+    /*
+   public int myInt = 5;
+   void Start()
+   {
+       myInt = MultiplyByTwo(myInt); 
+       Debug.Log(myInt);
+   }
+   
+   int MultiplyByTwo(public int number)
+   {
+      int result;
+      result = number * 2;
+      return ret;
+   } */
+}
+// I don't understand very much but the comments make sense
