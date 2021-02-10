60e96bb1f59f916b2cd4994f95f35063ac310471
Cossette <10838257@my.uvu.edu>
2021-02-09 17:07:15 -0800

    Working Counter, Loop scripts

---
+++ Super Awesome Car Game/Assets/Scripts/Counter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int scoreCount;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreCount++;
+        print(scoreCount);
+    }
+
+    void ScoreTest()
+    {
+        if (scoreCount > 100)
+        {
+            print("Gold Star for You");
+        }
+        else
+        {
+            print("Go for the Gold");
+        }
+    }
+}
---
+++ Super Awesome Car Game/Assets/Scripts/CupsLoops.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DishwashingLoops : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int cupsInTheSink = 4;
+        int platesInTheSink = 3;
+        int chopsticksInTheSink = 6;
+        while (cupsInTheSink > 0)
+        {
+            Debug.Log("I've washed a cup");
+            cupsInTheSink--;
+        }
+
+        bool shouldContinue = false;
+
+        do
+        {
+            Debug.Log("I'm washing plates");
+        } 
+        while (shouldContinue == true);
+
+        for (int i = 0; i < chopsticksInTheSink; i--)
+        {
+            Debug.Log("I'm also washing chopstick number" + i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
