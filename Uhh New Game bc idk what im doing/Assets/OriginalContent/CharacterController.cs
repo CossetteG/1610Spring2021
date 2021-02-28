91e59e41c107332362b0c589b07dd9d2fa0aabe8
Cossette <10838257@my.uvu.edu>
2021-02-28 12:02:24 -0800

    ??????? movement script

--- Uhh New Game bc idk what im doing/Assets/OriginalContent/CharacterController.cs
+++ Uhh New Game bc idk what im doing/Assets/OriginalContent/CharacterController.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    public Rigidbody2D rigidbodyObj;
+    public float speed = 10f;
+    public float jumpForce = 5f;
     
+    private Vector2 direction;
     void FixedUpdate()
     {
-        Input
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        //Direction.x =(speed * moveHorizontal);
+        //RigidbodyObj.addForce(Direction, ForceMode2D.force);
     }
 }
